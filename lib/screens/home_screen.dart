34e0875110bc0eb03a7dc62b6e666513875b1acb
BeSalt1080 <wijayashinichi@gmail.com>
2025-10-05 21:28:31 +0700

    fix: remove duplicate import of go_router in home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:go_router/go_router.dart';
 import '../controllers/product_controller.dart';
 import '../models/product.dart';
 import '../widgets/product_card.dart';
